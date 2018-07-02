e3bced8f5dd4f26655e876122600820235bca257
Keanu Lee <keanulee@google.com>
2018-07-02 14:00:50 -0700

    Manual updates for 3.0

--- paper-spinner-lite.js
+++ paper-spinner-lite.js
@@ -1,55 +1,21 @@
 /**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-/**
-Material design: [Progress & activity](https://www.google.com/design/spec/components/progress-activity.html)
-
-Element providing a single color material design circular spinner.
-
-    <paper-spinner-lite active></paper-spinner-lite>
-
-The default spinner is blue. It can be customized to be a different color.
-
-### Accessibility
-
-Alt attribute should be set to provide adequate context for accessibility. If not provided,
-it defaults to 'loading'.
-Empty alt can be provided to mark the element as decorative if alternative content is provided
-in another form (e.g. a text block following the spinner).
-
-    <paper-spinner-lite alt="Loading contacts list" active></paper-spinner-lite>
-
-### Styling
-
-The following custom properties and mixins are available for styling:
-
-Custom property | Description | Default
-----------------|-------------|----------
-`--paper-spinner-color` | Color of the spinner | `--google-blue-500`
-`--paper-spinner-stroke-width` | The width of the spinner stroke | 3px
-
-@group Paper Elements
-@element paper-spinner-lite
-@hero hero.svg
-@demo demo/index.html
-*/
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
+This code may only be used under the BSD style license found at
+http://polymer.github.io/LICENSE.txt The complete set of authors may be found at
+http://polymer.github.io/AUTHORS.txt The complete set of contributors may be
+found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by Google as
+part of the polymer project is also subject to an additional IP rights grant
+found at http://polymer.github.io/PATENTS.txt
 */
 import '@polymer/polymer/polymer-legacy.js';
-
 import '@polymer/paper-styles/color.js';
-import { PaperSpinnerBehavior } from './paper-spinner-behavior.js';
 import './paper-spinner-styles.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+
+import {Polymer} from '@polymer/polymer/lib/legacy/polymer-fn.js';
+
+import {PaperSpinnerBehavior} from './paper-spinner-behavior.js';
+
 const $_documentContainer = document.createElement('template');
 $_documentContainer.setAttribute('style', 'display: none;');
 
@@ -69,6 +35,39 @@ $_documentContainer.innerHTML = `<dom-module id="paper-spinner-lite">
 </dom-module>`;
 
 document.head.appendChild($_documentContainer.content);
+/**
+Material design: [Progress &
+activity](https://www.google.com/design/spec/components/progress-activity.html)
+
+Element providing a single color material design circular spinner.
+
+    <paper-spinner-lite active></paper-spinner-lite>
+
+The default spinner is blue. It can be customized to be a different color.
+
+### Accessibility
+
+Alt attribute should be set to provide adequate context for accessibility. If
+not provided, it defaults to 'loading'. Empty alt can be provided to mark the
+element as decorative if alternative content is provided in another form (e.g. a
+text block following the spinner).
+
+    <paper-spinner-lite alt="Loading contacts list" active></paper-spinner-lite>
+
+### Styling
+
+The following custom properties and mixins are available for styling:
+
+Custom property | Description | Default
+----------------|-------------|----------
+`--paper-spinner-color` | Color of the spinner | `--google-blue-500`
+`--paper-spinner-stroke-width` | The width of the spinner stroke | 3px
+
+@group Paper Elements
+@element paper-spinner-lite
+@hero hero.svg
+@demo demo/index.html
+*/
 Polymer({
   is: 'paper-spinner-lite',
 
